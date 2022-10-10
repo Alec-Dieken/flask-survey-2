0615e327b685f30e8d696e708d4fc4fd1a543549
Alec Dieken <alec@alecdieken.com>
2022-10-09 22:15:26 -0700

    added all exercise files

--- app.py
+++ app.py
@@ -1,35 +1,40 @@
 from flask import Flask, render_template, request, redirect, flash
+from flask import session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import *
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'my secret'
 
-# debug = DebugToolbarExtension(app)
-
-responses = []
 
 @app.route('/')
 def startpage():
     return render_template('start-page.html')
 
+@app.route('/start', methods=["POST"])
+def startsession():
+    session['responses'] = []
+    return redirect('/questions/0')
+
 @app.route('/questions/<int:q_num>')
 def show_question(q_num):
-    if not q_num == len(responses):
+    if not q_num == len(session['responses']):
         flash("Answer previous questions first")
-        return redirect(f'/questions/{len(responses)}')
+        return redirect(f'/questions/{len(session["responses"])}')
     question = satisfaction_survey.questions[q_num].question
     choices = satisfaction_survey.questions[q_num].choices
-    button_text = "Next Question" if len(responses) < len(satisfaction_survey.questions) - 1 else "Finish"
+    button_text = "Next Question" if len(session['responses']) < len(satisfaction_survey.questions) - 1 else "Finish"
     return render_template('question.html', question=question, choices=choices, button_text=button_text)
 
 @app.route('/answer', methods=["POST"])
 def answer_post():
     answer = request.form
-    responses.append(answer['choice'])
-    if(len(responses) == len(satisfaction_survey.questions)):
+    new_responses = session['responses']
+    new_responses.append(answer['choice'])
+    session['responses'] = new_responses
+    if len(session['responses']) == len(satisfaction_survey.questions):
         return redirect('thank-you')
-    return redirect(f'/questions/{len(responses)}')
+    return redirect(f'/questions/{len(session["responses"])}')
 
 @app.route('/thank-you')
 def thanks():
